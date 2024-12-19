904926c15f509a76b3145f5906a971b1edbd22ff
devendra2027 <devendrakurve094@gmail.com>
2024-12-19 13:55:41 +0530

    nskj

---
+++ dec_2_sorting_technics/merge_sort.cpp
@@ -0,0 +1,70 @@
+// 18/12/2024
+#include <iostream>
+using namespace std;
+void merge(int A[], int low, int mid, int high)
+{
+    int i, j, k, B[high + 1];
+    i = low;
+    j = mid + 1;
+    k = low;
+    while (i <= mid && j <= high)
+    {
+        if (A[i] < A[j])
+        {
+            B[k] = A[i];
+            i++;
+        }
+        else
+        {
+            B[k] = A[j];
+            j++;
+        }
+        k++;
+    }
+    while (i <= mid)
+    {
+        B[k] = A[i];
+        k++;
+        i++;
+    }
+    while (j <= high)
+    {
+        B[k] = A[j];
+        k++;
+        j++;
+    }
+    for (i = low; i <= high; i++)
+    {
+        A[i] = B[i];
+    }
+}
+void divide(int A[], int low, int high)
+{
+    int mid;
+    if (low < high)
+    {
+        mid = (low + high) / 2;
+        divide(A, low, mid);
+        divide(A, mid + 1, high);
+        merge(A, low, mid, high);
+    }
+}
+void printarr(int A[], int n)
+{
+    int i;
+    for (i = 0; i < n; i++)
+    {
+        cout << A[i] << "\t ";
+    }
+    cout << "\n";
+}
+
+int main()
+{
+    int A[] = {3, 5, 2, 13, 12, 10, 6};
+    int n = 7;
+    printarr(A, n);
+    divide(A, 0, n - 1);
+    cout << "Sorted array is \n";
+    printarr(A, n);
+}
--- dec_2_sorting_technics/quick_sort.cpp
+++ dec_2_sorting_technics/quick_sort.cpp
@@ -1,7 +1,60 @@
-// 12/12/2024 quick sort 
-#include<iostream>
+// 12/12/2024 quick sort
+#include <iostream>
 using namespace std;
+int part(int arr[], int low, int high)
+{
+    int pv;
+    pv = arr[low];
+    int i = low + 1;
+    int j = high;
+    do
+    {
+        while (arr[i] < pv)
+        {
+            i++;
+        }
+        while (arr[j] > pv)  // stop lesser than equal to pivot value
+        {
+            j--;
+        }
+        if (i < j)
+        {
+            int temp;
+            temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    } while (i < j);
+
+    int temp;
+    temp = arr[low];
+    arr[low] = arr[j];
+    arr[j] = temp;
+    return j;
+} 
+void QKSORT(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+        int pv;
+        pv = part(arr, low, high);
+        QKSORT(arr, low, pv - 1);
+        QKSORT(arr, pv + 1, high);
+    }
+}
+void show(int arr[], int s)
+{
+    for (int i = 0; i < s; i++)
+    {
+        cout << arr[i] << "\n";
+    }
+}
 int main()
 {
-    
+    int arr[]= {9,2,9,4,1};
+    int s= sizeof(arr)/sizeof(arr[0]);
+    show (arr,s);
+    QKSORT(arr,0,s-1);
+    cout<<"after applying quick sort arry";
+    show(arr,s);
 }
