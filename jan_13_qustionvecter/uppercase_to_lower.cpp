ce35bb960595f35cae5aac5411e821624333d26e
devendra2027 <devendrakurve094@gmail.com>
2025-01-15 19:30:36 +0530

    wsdf

--- jan_13_qustionvecter/uppercase_to_lower.cpp
+++ jan_13_qustionvecter/uppercase_to_lower.cpp
@@ -1,19 +1,44 @@
+// #include<iostream>
+// using namespace std;
+// #include<bits/stdc++.h>
+
+// int main()
+// {
+//     string sen;
+//     cout<<"enter uper case=";
+//     getline(cin,sen);
+    
+
+//     for(int i=0;i<sen.size();i++)
+//     {
+//         if(sen[i]>='A' && sen[i]<='Z')
+//             sen[i]+=32;
+//     }
+//     cout<<"result="<<sen;
+//     return 0;
+// }
+
+
+
 #include<iostream>
 using namespace std;
 #include<bits/stdc++.h>
 
 int main()
 {
-    string sen;
-    cout<<"enter uper case=";
-    getline(cin,sen);
-    
-
-    for(int i=0;i<sen.size();i++)
+    string s="Th089i*S";
+    for(int i=0;i<s.size();i++)
     {
-        if(sen[i]>='A' && sen[i]<='Z')
-            sen[i]+=32;
+        if(s[i]>='A' && s[i]<='Z')
+        {
+            char c=s[i]+32;
+            cout<<c;
+        }
+        else if(s[i]>='a' && s[i]<='z')
+        {
+            cout<<s[i];
+        }
+            
     }
-    cout<<"result="<<sen;
-    return 0;
+  
 }
