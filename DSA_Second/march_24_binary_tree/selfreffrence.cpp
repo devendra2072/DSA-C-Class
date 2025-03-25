47b4a9dd61e22bc4fb216ee23bdf8fac6c71bdce
devendra2027 <devendrakurve094@gmail.com>
2025-03-25 16:07:51 +0530

    asdfg

---
+++ DSA_Second/march_24_binary_tree/selfreffrence.cpp
@@ -0,0 +1,86 @@
+// #include <iostream>
+// using namespace std;
+// class node
+// {
+//     public:int data;
+//     node *l;
+//     node *r;
+// public:
+//     node(int d)
+//     {
+//         data = d;
+//         l = NULL;
+//         r = NULL;
+//     }
+// };
+// int main()
+// {
+//     node *root = new node(10);
+//     root->l = new node(20);
+//     root->r = new node(30);
+//     cout<<root->data<<"\n";
+//     cout<<root->l->data<<"\n";
+//     cout<<root->r->data<<"\n";
+// }
+
+
+
+// function formate 
+
+#include <iostream>
+using namespace std;
+class node
+{
+    public:int data;
+    node *l;
+    node *r;
+public:
+    node(int d)
+    {
+        data = d;
+        l = NULL;
+        r = NULL;
+    }
+};
+void preorder(node *root)
+{
+    if(root!= NULL)
+    {
+        return;
+    }
+    cout<<root->data<<"\n";
+    preorder(root->l);
+    preorder(root->r);
+}
+void postorder(node *root)
+{
+    if(root!= NULL)
+    {
+        return;
+    }
+    postorder(root->l);
+    postorder(root->r);
+    cout<<root->data<<"\n";
+}
+void inorder(node *root)
+{
+    if(root!= NULL)
+    {
+        return;
+    }
+    inorder(root->l);
+    cout<<root->data<<"\n";
+    inorder(root->r);
+}
+int main()
+{
+    node *root = new node(10);
+    root->l = new node(20);
+    root->r = new node(30);
+    cout<<root->data<<"\n";
+    cout<<root->l->data<<"\n";
+    cout<<root->r->data<<"\n";
+    preorder(root);
+    postorder(root);
+    inorder(root);
+}
